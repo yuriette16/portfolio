c1b6a291f10799658e9c705bcfb0127c93952397
Yurie Shiotani <yurieshiotani@gmail.com>
2020-09-13 17:06:54 +0900

    Project modal initial

--- src/components/cards/Project.js
+++ src/components/cards/Project.js
@@ -1,21 +1,36 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
 import '../../style/Project.css';
 
 class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
 
     render () {
 
         const { projectName, projectSkill, projectRole, projectDescription, projectGif } = this.props;
 
-        const displayProjectDetail = () => {
-            console.log(projectSkill);
-            
+        const displayProjectModal = () => {
+             this.setState({
+                open: true  
+             });
+        };
+
+        const closeProjectModal = () => {
+            this.setState({
+                open: false 
+             });
         };
 
         return (
 
-            <div className="project" onClick={displayProjectDetail} >
+            <div className="project" onClick={displayProjectModal} >
                 <div>{ projectSkill }</div>
                 <div>{ projectRole }</div>
                 <div>{ projectDescription }</div>
@@ -23,6 +38,17 @@ class Project extends Component {
                     <div className="project-name">{ projectName }</div>
                     <img src={ projectGif } className="project-gif"></img>
                 </div>
+                <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+          
+                    open={ this.state.open }
+                    onClose={ closeProjectModal }
+
+                >
+
+                            <h2 id="transition-modal-title">{ projectName }</h2>
+                </Modal>
             </div>
 
         );
